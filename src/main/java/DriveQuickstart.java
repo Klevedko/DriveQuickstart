53fe059740e92796078de4179f2d265124132332
akrasilnikov <meingipfel@mail.ru>
2018-08-28 15:51:16 +0300

    root to folder ID

--- src/main/java/DriveQuickstart.java
+++ src/main/java/DriveQuickstart.java
@@ -4,259 +4,78 @@ import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
 import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
 import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
-import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
-import com.google.api.services.appsactivity.Appsactivity;
-import com.google.api.services.appsactivity.AppsactivityScopes;
-import com.google.api.services.appsactivity.model.*;
-import com.google.api.services.appsactivity.model.User;
-import org.apache.poi.hssf.usermodel.HSSFWorkbook;
-import org.apache.poi.ss.usermodel.Cell;
-import org.apache.poi.ss.usermodel.Row;
-import org.apache.poi.ss.usermodel.Sheet;
-import org.json.JSONArray;
-import org.json.JSONObject;
+import com.google.api.services.admin.reports.Reports;
+import com.google.api.services.admin.reports.ReportsScopes;
+import com.google.api.services.admin.reports.model.Activities;
+import com.google.api.services.admin.reports.model.Activity;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.util.*;
+import java.security.GeneralSecurityException;
+import java.util.Collections;
+import java.util.List;
 
 public class DriveQuickstart {
-    /**
-     * Application name.
-     */
-    private static final String APPLICATION_NAME =
-            "G Suite Activity API Java Quickstart";
-
-    /**
-     * Directory to store authorization tokens for this application.
-     */
-    private static final java.io.File DATA_STORE_DIR = new java.io.File("tokens");
-
-    /**
-     * Global instance of the {@link FileDataStoreFactory}.
-     */
-    private static FileDataStoreFactory DATA_STORE_FACTORY;
-
-    /**
-     * Global instance of the JSON factory.
-     */
-    private static final JsonFactory JSON_FACTORY =
-            JacksonFactory.getDefaultInstance();
-
-    /**
-     * Global instance of the HTTP transport.
-     */
-    private static HttpTransport HTTP_TRANSPORT;
+    private static final String APPLICATION_NAME = "Google Admin SDK Reports API Java Quickstart";
+    private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
+    private static final String TOKENS_DIRECTORY_PATH = "tokens";
 
     /**
      * Global instance of the scopes required by this quickstart.
-     * <p>
-     * If modifying these scopes, delete your previously saved credentials
-     * at ~/.credentials/appsactivity-java-quickstart
+     * If modifying these scopes, delete your previously saved tokens/ folder.
      */
-    private static final List<String> SCOPES = Arrays.asList(AppsactivityScopes.ACTIVITY);
-
-    static {
-        try {
-            HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
-            DATA_STORE_FACTORY = new FileDataStoreFactory(DATA_STORE_DIR);
-        } catch (Throwable t) {
-            t.printStackTrace();
-            System.exit(1);
-        }
-    }
+    private static final List<String> SCOPES = Collections.singletonList(ReportsScopes.ADMIN_REPORTS_AUDIT_READONLY);
+    private static final String CREDENTIALS_FILE_PATH = "/credentials.json";
 
     /**
      * Creates an authorized Credential object.
-     *
-     * @return an authorized Credential object.
-     * @throws IOException
+     * @param HTTP_TRANSPORT The network HTTP Transport.
+     * @return An authorized Credential object.
+     * @throws IOException If the credentials.json file cannot be found.
      */
-    public static String history = "";
-    public static String historyAdd = "";
-    public static String historyDel = "";
-    public static String historyRem = "";
-    public static String his = "";
-    public static JSONArray geodata;
-
-    public static Credential authorize() throws IOException {
+    private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
         // Load client secrets.
-        InputStream in =
-                DriveQuickstart.class.getResourceAsStream("/credentials.json");
-        GoogleClientSecrets clientSecrets =
-                GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
+        InputStream in = DriveQuickstart.class.getResourceAsStream(CREDENTIALS_FILE_PATH);
+        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
         // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow =
-                new GoogleAuthorizationCodeFlow.Builder(
-                        HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                        .setDataStoreFactory(DATA_STORE_FACTORY)
-                        .setAccessType("offline")
-                        .build();
-        Credential credential = new AuthorizationCodeInstalledApp(
-                flow, new LocalServerReceiver()).authorize("user");
-        System.out.println(
-                "Credentials saved to " + DATA_STORE_DIR.getAbsolutePath());
-        return credential;
+        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
+                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
+                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
+                .setAccessType("offline")
+                .build();
+        return new AuthorizationCodeInstalledApp(flow, new LocalServerReceiver()).authorize("user");
     }
 
-    /**
-     * Build and return an authorized Apps Activity client service.
-     *
-     * @return an authorized Appsactivity client service
-     * @throws IOException
-     */
-    public static Appsactivity getAppsactivityService() throws IOException {
-        Credential credential = authorize();
-        return new Appsactivity.Builder(
-                HTTP_TRANSPORT, JSON_FACTORY, credential)
+    public static void main(String... args) throws IOException, GeneralSecurityException {
+        // Build a new authorized API client service.
+        final NetHttpTransport HTTP_TRANSPORT = GoogleNetHttpTransport.newTrustedTransport();
+        Reports service = new Reports.Builder(HTTP_TRANSPORT, JSON_FACTORY, getCredentials(HTTP_TRANSPORT))
                 .setApplicationName(APPLICATION_NAME)
                 .build();
-    }
 
-    public static void main(String[] args) throws IOException {
-        Appsactivity service = getAppsactivityService();
-
-        ListActivitiesResponse result = service.activities().list()
-                .setSource("drive.google.com")
-                .setDriveAncestorId("1hFVqvIGHinC-cbu5_Cbt_DKxWdNtweRv")
-                .setPageSize(111)
+        // Print the last 10 login events.
+        String userKey = "all";
+        String applicationName = "login";
+        Activities result = service.activities().list(userKey, applicationName)
+                .setMaxResults(10)
                 .execute();
-
-        List<Activity> activities = result.getActivities();
+        List<Activity> activities = result.getItems();
         if (activities == null || activities.size() == 0) {
-            System.out.println("No activity.");
+            System.out.println("No logins found.");
         } else {
-            ArrayList<Employee> al = new ArrayList<Employee>();
-            System.out.println("Recent activity:");
-
+            System.out.println("Logins:");
             for (Activity activity : activities) {
-
-                Event event = activity.getCombinedEvent();
-                String date = new java.text.SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ssZ")
-                        .format(new java.util.Date(event.getEventTimeMillis().longValue()));
-
-                User user = event.getUser();
-                Target target = event.getTarget();
-                if (user == null || target == null ) {
-                    continue;
-                }
-                String evlist_string = "";
-                System.out.printf("%s: %s. FILE: %s,  ACTION: %s. GETPERMISSIONCHANGES_JSON %s\n",
-                        date,
-                        user.getName(),
-                        target.getName(),
-                        event.getPrimaryEventType(),
-                        event.getPermissionChanges()
-                );
-
-                List<PermissionChange> evlist = event.getPermissionChanges();
-                if (!(evlist == null)) {
-                    for (PermissionChange permissionChange : evlist) {
-                        evlist_string = evlist_string + permissionChange;
-                    }
-                    if (event.getPrimaryEventType().equals("permissionChange") || event.getPrimaryEventType().equals("create")) {
-                        System.out.println(event.getPrimaryEventType());
-                        JSONObject obj = new JSONObject(evlist_string);
-                        try {
-                            geodata = obj.getJSONArray("addedPermissions");
-                            //System.out.println("add" + geodata);
-                            historyAdd = "addedPermissions:\n" + getHistory(geodata);
-
-                        } catch (Exception e) {
-                            //System.out.println(e.getLocalizedMessage());
-                        }
-
-                        try {
-                            geodata = obj.getJSONArray("deletedPermissions");
-                            //System.out.println("del" + geodata);
-                            historyDel = "deletedPermissions:\n" + getHistory(geodata);
-                        } catch (Exception e) {
-                            //System.out.println(e.getLocalizedMessage());
-                        }
-
-                        try {
-                            geodata = obj.getJSONArray("removedPermissions");
-                            historyRem = "removedPermissions:\n" + getHistory(geodata);
-                            //System.out.println("rem" + geodata);
-                        } catch (Exception e) {
-                            //System.out.println(e.getLocalizedMessage());
-                        }
-                    }
-                    history = historyAdd.concat(historyDel.concat(historyRem));
-                    al.add(new Employee(date, user.getName(), target.getName(), event.getPrimaryEventType(), history));
-                    clearAll();
-                }
+                System.out.printf("%s: %s (%s)\n",
+                        activity.getId().getTime(),
+                        activity.getActor().getEmail(),
+                        activity.getEvents().get(0).getName());
             }
-            Collections.sort(al);
-            write_to_file(al);
         }
-    }
-
-    public static String getHistory(JSONArray geodata) {
-        final int n = geodata.length();
-        for (int i = 0; i < n; ++i) {
-            JSONObject person = geodata.getJSONObject(i);
-            his = person.getString("name") + " : " + person.getString("role") + "\n";
-        }
-        return his;
-    }
-
-    public static void clearAll() {
-        history = "";
-        historyDel = "";
-        historyAdd = "";
-        historyRem = "";
-    }
-
-    public static void write_to_file(ArrayList<Employee> al) {
-        try {
-            HSSFWorkbook wb = new HSSFWorkbook();
-            Cell cell;
-            Sheet list = wb.createSheet("SheetName");
-            byte row = 0;
-            String output = "audit_results.xls";
-
-            FileOutputStream fileout;
-            fileout = new FileOutputStream(output);
-
-            Row dataRow = list.createRow(row);
-            cell = dataRow.createCell(0);
-            cell.setCellValue("Date");
-            cell = dataRow.createCell(1);
-            cell.setCellValue("Who");
-            cell = dataRow.createCell(2);
-            cell.setCellValue("File");
-            cell = dataRow.createCell(3);
-            cell.setCellValue("Action");
-            cell = dataRow.createCell(4);
-            cell.setCellValue("Activities");
-            row++;
-
-            for (Employee product : al) {
-                dataRow = list.createRow(row);
-                cell = dataRow.createCell(0);
-                cell.setCellValue(product.getDate());
-                cell = dataRow.createCell(1);
-                cell.setCellValue(product.getName());
-                cell = dataRow.createCell(2);
-                cell.setCellValue(product.getTarget_name());
-                cell = dataRow.createCell(3);
-                cell.setCellValue(product.getGet1());
-                cell = dataRow.createCell(4);
-                cell.setCellValue(product.getGet2());
-                row++;
-            }
-
-            wb.write(fileout);
-            fileout.close();
-        } catch (Exception e) {
-        }
-
     }
 }
