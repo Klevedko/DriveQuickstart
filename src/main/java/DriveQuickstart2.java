f87ff343246fde54ee7e52a06e428696f2aba6c4
akrasilnikov <meingipfel@mail.ru>
2018-08-31 09:25:44 +0300

    root to folder ID

--- src/main/java/DriveQuickstart2.java
+++ src/main/java/DriveQuickstart2.java
@@ -8,41 +8,40 @@ import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.store.FileDataStoreFactory;
+import com.google.api.services.appsactivity.model.Parent;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
-import com.google.api.services.drive.model.Change;
-import com.google.api.services.drive.model.ChangeList;
-import com.google.api.services.drive.model.File;
-import com.google.api.services.drive.model.FileList;
+import com.google.api.services.drive.model.*;
 
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
+import java.security.Permission;
 import java.util.Collections;
 import java.util.List;
 
 public class DriveQuickstart2 {
     private static final String APPLICATION_NAME = "Google Drive API Java Quickstart";
     private static final JsonFactory JSON_FACTORY = JacksonFactory.getDefaultInstance();
-    private static final String TOKENS_DIRECTORY_PATH = "tokens";
+    private static final String TOKENS_DIRECTORY_PATH = "tokensMaster";
 
     /**
      * Global instance of the scopes required by this quickstart.
      * If modifying these scopes, delete your previously saved credentials/ folder.
      */
     private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE_METADATA_READONLY);
-    private static final String CREDENTIALS_FILE_PATH = "C:/IdeaProjects/DriveQuickstart/src/main/resources/credentials.json";
 
     /**
      * Creates an authorized Credential object.
+     *
      * @param HTTP_TRANSPORT The network HTTP Transport.
      * @return An authorized Credential object.
      * @throws IOException If the credentials.json file cannot be found.
      */
     private static Credential getCredentials(final NetHttpTransport HTTP_TRANSPORT) throws IOException {
         // Load client secrets.
-        InputStream in = DriveQuickstart.class.getResourceAsStream("credentials.json");
+        InputStream in = DriveQuickstart.class.getResourceAsStream("credentialsMaster.json");
         GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(in));
 
         // Build flow and trigger user authorization request.
@@ -73,6 +72,7 @@ public class DriveQuickstart2 {
         } else {
             System.out.println("Files:");
             for (File file : files) {
+
                 System.out.printf("%s (%s)\n", file.getName(), file.getId());
             }
         }
@@ -83,7 +83,8 @@ public class DriveQuickstart2 {
                 .execute();
         for (Change change : changes.getChanges()) {
             // Process change
-            System.out.println("Change found for file: " + change.getFileId() );
+            System.out.println("Change found for file: " + change.getFileId());
+
         }
         /*if (changes.getNewStartPageToken() != null) {
             // Last page, save this token for the next polling interval
